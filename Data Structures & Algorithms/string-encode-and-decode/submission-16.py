f612e6f4fc103b0e6053333a81bb8083aa937abf
Balatro_ <146581521+Suicide-Vest-Enjoyer@users.noreply.github.com>
2026-07-11 20:47:25 +0200

    Add: string-encode-and-decode - submission-16

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-16.py
@@ -0,0 +1,39 @@
+#class Solution:
+#    def encode(self, strs: List[str]) -> str:
+#        result=""
+#        for i in strs:
+#            result+=str(len(i))+"#"+i
+#        return result
+#
+#    def decode(self, s: str) -> List[str]:
+#        left=0
+#        result=[]
+#        while left<len(s):
+#            right=left
+#            while s[right]!="#":
+#                right+=1
+#            val=int(s[left:right])
+#            result.append(str(s[right+1:right+1+val]))
+#            left=right+1+val
+#        return result
+#
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        res=""
+        for i in strs:
+            res+=str(len(i))+"#"+i
+        return res
+
+
+    def decode(self, s: str) -> List[str]:
+        i=0
+        res=[]
+        while i<len(s):
+            j=i
+            while s[j]!="#":
+                j+=1
+            val=int(s[i:j])
+            res.append(str(s[j+1:j+1+val]))
+            i=j+1+val
+        return res
