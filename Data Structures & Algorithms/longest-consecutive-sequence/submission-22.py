9bb7c573761623055b83a4714e75c4e5e65a694a
Balatro_ <146581521+Suicide-Vest-Enjoyer@users.noreply.github.com>
2026-07-13 13:31:19 +0200

    Add: longest-consecutive-sequence - submission-22

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-22.py
@@ -0,0 +1,48 @@
+#class Solution:
+#    def longestConsecutive(self, nums: List[int]) -> int:
+#        nums = set(nums)
+#        nums = list(nums)
+#        nums.sort()
+#        best = 0
+#        cur = 0
+#        for i, v in enumerate(nums):
+#            if i == 0 or nums[i-1] == v - 1:
+#                cur += 1
+#                best = max(best, cur)
+#            else:
+#                cur = 1
+#        
+#        return best
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        nums = set(nums)
+        best = 1
+        lenght = 1
+
+        if len(nums) == 0:
+            return 0
+
+        for num in nums:
+            if num - 1 in nums:
+                num = num - 1
+                lenght = 1
+                continue
+            
+            cur = num
+
+            while cur + 1 in nums:
+                cur += 1
+                lenght += 1
+
+            best = max(lenght, best)
+
+        return best
+
+
+
+
+
+
+
+
