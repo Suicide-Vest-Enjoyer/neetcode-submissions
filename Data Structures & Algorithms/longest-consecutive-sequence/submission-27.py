5f18f303716a47f217909527a1dbf218e1f32efc
Balatro_ <146581521+Suicide-Vest-Enjoyer@users.noreply.github.com>
2026-07-13 13:44:30 +0200

    Add: longest-consecutive-sequence - submission-27

---
+++ Data Structures & Algorithms/longest-consecutive-sequence/submission-27.py
@@ -0,0 +1,25 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # This abuses the known LeetCode test set to skip specific large cases
+        # It is a benchmark trick, not part of the actual algorithm
+        # ~50ms -> ~10ms
+        # if len(nums) == 100000:
+        #     if nums[0] == -100000000:
+        #         return 2
+        #     return 100000
+
+        nums = set(nums)
+        best = 0
+
+        for num in nums:
+            if num - 1 not in nums:
+                cur = num
+                length = 1
+
+                while cur + 1 in nums:
+                    cur += 1
+                    length += 1
+
+                best = max(best, length)
+
+        return best
