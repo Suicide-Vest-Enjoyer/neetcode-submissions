405901db4c69cf16c391936e11036718a9cac962
Balatro_ <146581521+Suicide-Vest-Enjoyer@users.noreply.github.com>
2026-09-05 16:44:10 +0200

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        seen = []
+        valid = {
+            ")": "(",
+            "}": "{", 
+            "]": "["
+        }
+        
+        for i in s:
+            if i in valid.values():
+                seen.append(i)
+            elif seen != [] and seen[-1] == valid.get(i):
+                seen.pop()
+            else:
+                return False
+        
+        return seen == []
