02b3cfd4a8d5de0762efb2920c8975884f570dc0
Balatro_ <146581521+Suicide-Vest-Enjoyer@users.noreply.github.com>
2026-09-10 17:42:53 +0200

    Add: reorder-linked-list - submission-1

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-1.py
@@ -0,0 +1,33 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            
+        prev = None
+        curr = slow.next
+        slow.next = None
+
+        while curr:
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
+
+        # 3. Merge two halves
+        first = head
+        second = prev
+
+        while second:
+            first_next = first.next
+            second_next = second.next
+
+            first.next = second
+            second.next = first_next
+
+            first = first_next
+            second = second_next
